c8bae150e62ab6d837cd7e8f1d4d80563b2f031f
Marvin Langer <info@motion-m.de>
2019-03-12 13:35:51 +0100

    removed test console logs / final

--- resources/js/prideaproducts.js
+++ resources/js/prideaproducts.js
@@ -6,10 +6,8 @@ jQuery(document).ready(function() {
 		setTimeout(function(){
 			if( jQuery("#checkoutButtonPayPalInstallment").text() == "Ratenzahlung beantragen" ){
 				jQuery("#paypal-installment-specific-promotion_").css("display","block");
-				console.log("finanzierungsmöglichen anzeigen");
 			}else{
 				jQuery("#paypal-installment-specific-promotion_").css("display","none");
-				console.log("finanzierungsmöglichen verbergen");
 			}
 		},500);
 	}
