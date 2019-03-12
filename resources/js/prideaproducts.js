e44541449619a7b74b6b60168f8342da2254341a
Marvin Langer <info@motion-m.de>
2019-03-12 12:43:17 +0100

    paypal ratenzahlung css + js functions

--- resources/js/prideaproducts.js
+++ resources/js/prideaproducts.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function() {
 	
+	jQuery(".checkout .paypal-installment-specific-promotion").css("display","none");
+
+	function checkFinanzierungsanzeige(){
+		if( jQuery("#PP_Installments").hasClass("selected") ){
+			jQuery(".checkout .paypal-installment-specific-promotion").css("display","block");
+			alert("leider nein");
+		}else{
+			jQuery(".checkout .paypal-installment-specific-promotion").css("display","none");
+			alert("leider ja");
+		}
+	}
+
+	jQuery("#paymentMethodContainer").click(function(){
+		checkFinanzierungsanzeige();
+	});
 
 });
